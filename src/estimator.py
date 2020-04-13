b21f5de2ee9ec17b8adb7d0e06c17bfd26e73075
Mercy Tum <mercytum@gmail.com>
2020-04-13 23:52:10 +0300

    Update estimator.py
    Challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,41 @@
+import datetime
+
 def estimator(data):
+  impact = {}
+  severeImpact = {}
+  factor = 3
+  futureDate = datetime.date(2020,5,13)
+  today = datetime.date.today()
+  days = (futureDate - today).days
+
+  impact.currentlyInfected = data.reportedCases * 10
+  severeImpact.currentlyInfected = data.reportedCases * 50
+
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** (days/factor))
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** (days/factor))
+
   return data
+
+
+
+# data = {
+#   'region': {
+#     'name': 'Africa',
+#     'avgAge': 19.7,
+#     'avgDailyIncomeInUSD': 5,
+#     'avgDailyIncomePopulation': 0.71
+#     },
+#   'periodType': 'days',
+#   'timeToElapse': 58,
+#   'reportedCases': 674,
+#   'population': 66622705,
+#   'totalHospitalBeds': 1380614
+# }
+
+#Expected output
+# {
+# data: {}, // the input data you got
+# impact: {}, // your best case estimation
+# severeImpact: {} // your severe case estimation
+# }
+
