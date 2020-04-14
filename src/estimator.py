2158cfaf9f3c7fc43e88c8ff7b613bc675cf2917
Mercy Tum <mercytum@gmail.com>
2020-04-14 21:22:43 +0300

    Update estimator.py
    challenge 2

--- src/estimator.py
+++ src/estimator.py
@@ -23,6 +23,23 @@ def estimator(data):
   impact['infectionsByRequestedTime'] = math.trunc(impact['currentlyInfected']) * (2 ** math.trunc((days/factor)))
   severeImpact['infectionsByRequestedTime'] = math.trunc(severeImpact['currentlyInfected']) * (2 ** math.trunc((days/factor)))
 
+  impact['severeCasesByRequestedTime'] = impact['infectionsByRequestedTime'] * 0.15
+  severeImpact['severeCasesByRequestedTime'] = severeImpact['infectionsByRequestedTime'] * 0.15
+
+  total_beds_available = math.trunc(data['totalHospitalBeds'] * 0.35)
+   
+  if total_beds_available >= impact['severeCasesByRequestedTime']:
+     impact['hospitalBedsByRequestedTime'] = total_beds_available
+  else:
+      impact['hospitalBedsByRequestedTime'] = total_beds_available - impact['severeCasesByRequestedTime']
+   
+  if total_beds_available >= severeImpact['severeCasesByRequestedTime']:
+     severeImpact['hospitalBedsByRequestedTime'] = total_beds_available
+  else:
+      severeImpact['hospitalBedsByRequestedTime'] = total_beds_available - severeImpact['severeCasesByRequestedTime']
+   
+
+
   output = {
     'data': data,
     'impact': impact,
@@ -32,21 +49,21 @@ def estimator(data):
 
   return output
 
-# data = {
-#   'region': {
-#     'name': 'Africa',
-#     'avgAge': 19.7,
-#     'avgDailyIncomeInUSD': 5,
-#     'avgDailyIncomePopulation': 0.71
-#     },
-#   'periodType': 'weeks',
-#   'timeToElapse': 58,
-#   'reportedCases': 674,
-#   'population': 66622705,
-#   'totalHospitalBeds': 1380614
-# }
+data = {
+  'region': {
+    'name': 'Africa',
+    'avgAge': 19.7,
+    'avgDailyIncomeInUSD': 5,
+    'avgDailyIncomePopulation': 0.71
+    },
+  'periodType': 'weeks',
+  'timeToElapse': 58,
+  'reportedCases': 674,
+  'population': 66622705,
+  'totalHospitalBeds': 1380614
+}
 
-# print(estimator(data))
+print(estimator(data))
 
 
 
